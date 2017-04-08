18d8fb7813baa6f8beffec41f9937f73fbc5e434
Joshua Kornfeld <jokornfeld@gmailcom>
2017-04-08 12:12:30 +0200

    Test angepasst

--- test_Reader.py
+++ test_Reader.py
@@ -6,9 +6,9 @@ class Test_test_Reader(unittest.TestCase):
 
     def test_reader_create(self):
 
-        a_reader = Reader("/home/admin/BackendTask/example_input.py")
+        a_reader = Reader("./example_input.py")
 
-        self.assertEquals(a_reader.path, '/home/admin/BackendTask/example_input.py')
+        self.assertEquals(a_reader.path, './example_input.py')
 	a_reader.ausgabe()
 
 if __name__ == '__main__':
