e8147d59afc3d40f4322af3ec2c6572f3ef0a9aa
Joshua Kornfeld <jokornfeld@gmailcom>
2017-04-08 19:14:01 +0200

    Close zum Builder hinzugefügt

--- JsonBuilder.py
+++ JsonBuilder.py
@@ -17,35 +17,55 @@ class JsonBuilder(object):
 	def list(self, key):
 		if (self.items[-1] is None):
                         self.items.append(Obj())
-		self.items[-1].items.append(List(key))
+		self.items.append(List(key))
 		return self
 
+	def close(self):
+		if (self.items[-1] is None):
+                        self.items.append(Obj())
+		val = ""
+		if (isinstance(self.items[-1], List)):
+			val = "]"
+		else:
+			val = "}"
+		self.items.append(Close(val))
+		return self
+	
 	def build(self):
 		x = ""
 		for item in self.items:
 			if (item is not None):
-				if (isinstance(item, Obj)):
-					x = "{"
-				else:
-					x = "["
-				if (self.pretty == 1):
-					x= x + "\n"
-
-				for kv in item.items:
-					if (isinstance(kv, KeyValue)):
-						x = x + self.__writekv(len(x),kv.key, kv.value)
+				if (isinstance(item, Obj) or isinstance(item, List)):
+					if (isinstance(item, Obj)):
+						x = x + "{"
 					else:
-						for kvl in kv.items:
-							 x = x + self.__writekv(len(x),kvl.key, kvl.value)
+						if ( 2 < len(x)):
+							x = x + ","
+							if (self.pretty ==1):
+								x = x + "\n"
+							else:
+								x = x + " "
+						
+						x = x + "\"" +  item.key + "\": ["
+					if (self.pretty == 1):
+						x= x + "\n"
+
+					for kv in item.items:
+						if(isinstance(kv, KeyValue)):	
+							x = x + self.__writekv(len(x),kv.key, kv.value)
+						else:			
+							index = 2			
+							for kvl in kv.items:
+								 print index
+								 x = x + self.__writekv(index,kvl.key, kvl.value)
+								 index = index + 1
 
 
-				if (self.pretty == 1):
-					x = x + "\n" 
+					if (self.pretty == 1):
+						x = x + "\n" 
 
-				if (isinstance(item, Obj)):
-                                	x = x + "}"
-                                else:
-                                	x = x + "]"
+				if (isinstance(item, Close)):
+                                	x = x + item.value
 
 
 		return x
@@ -61,6 +81,11 @@ class JsonBuilder(object):
 				x = x + ","
                         x = x + "\"" + key + "\": \"" + value + "\""
 		return x
+
+	def _classtest(self, cls):
+		if(isinstance(cls, KeyValue)):
+			return "kv"
+		return "list"
 			
 class Obj(object):
 
@@ -78,3 +103,9 @@ class List(object):
 	def __init__(self, key):
 		self.key = key
 		self.items = []
+
+class Close(object):
+	
+	def __init__(self, value):
+		self.value = value
+
--- test_JsonBuilder.py
+++ test_JsonBuilder.py
@@ -1,6 +1,8 @@
 import unittest
 
 from JsonBuilder import JsonBuilder
+from JsonBuilder import KeyValue
+from JsonBuilder import List
 
 class Test_test_JsonBuilder(unittest.TestCase):
 
@@ -8,17 +10,33 @@ class Test_test_JsonBuilder(unittest.TestCase):
                 a_builder = JsonBuilder(1)
                 self.assertEquals(a_builder.pretty, 1)
 
+	def test_builder_classtest(self):
+		a_builder = JsonBuilder(0)
+		a_keyvalue = KeyValue("a","b")
+		a_list = List("a")
+                print a_builder._classtest(a_keyvalue)
+		print a_builder._classtest(a_list)
+
 	def test_builder_simpel(self):
                 a_builder = JsonBuilder(0)
-                a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value")
+                a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
                 print a_builder.build()
-
 	
 	def test_builder_pretty_simple(self):
 		a_builder = JsonBuilder(1)
-		a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value")
+		a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
 		print a_builder.build()
 
+        def test_builder_advanced(self):
+                a_builder = JsonBuilder(0)
+                a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value").list("a_list").keyvalue("a_lkey", "alvalue").close().close()
+                print a_builder.build()
+
+
+        def test_builder_pretty_advanced(self):
+                a_builder = JsonBuilder(1)
+                a_builder.objekt().keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
+                print a_builder.build()
 
 if __name__ == '__main__':
     unittest.main()
