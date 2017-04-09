6231f6b0c5c0f4f9615d7e556da787016d5ed60a
Joshua Kornfeld <jokornfeld@gmailcom>
2017-04-09 10:59:26 +0200

    JsonBuilder fertiggestellt PropertySetToJsonComposer funtioniert als Konsolenausgabe

--- JsonBuilder.py
+++ JsonBuilder.py
@@ -1,39 +1,50 @@
 class JsonBuilder(object):
 
+	PRETTY_DIST = 4		
+
 	def __init__(self, pretty):
 	        self.pretty = pretty
+		self.level = 0
         	self.items = []
 
 	def objekt(self):
-		self.items.append(Obj())
+		if (self.__isNextNeed()):
+                        self.items.append(Next())
+		level = self.__findAndRaiseCurrentLevel()
+		self.items.append(Obj(level))
 		return self
+
+	def list(self, key):
+                if (self.__isObjNeed()):
+			level = self.__findAndRaiseCurrentLevel()
+                        self.items.append(Obj(level))
+                if (self.__isNextNeed()):
+                        self.items.append(Next())
+		level = self.__findAndRaiseCurrentLevel()
+                self.items.append(List(key, level))
+                return self
 
 	def value(self):
 		if (self.__isObjNeed()):
-                        self.items.append(Obj())
-                if (1 == self.__isNextNeed()):
+			level = self.__findAndRaiseCurrentLevel()
+                        self.items.append(Obj(level))
+                if (self.__isNextNeed()):
                         self.items.append(Next())
 		self.items.append(Value(value))
 
 	def keyvalue(self, key, value):
 		if (self.__isObjNeed()):
-			self.items.append(Obj())
-		if (1 == self.__isNextNeed()):
+			level = self.__findAndRaiseCurrentLevel()
+			self.items.append(Obj(level))
+		if (self.__isNextNeed()):
 			self.items.append(Next())
-		self.items.append(KeyValue(key,value))			
-		return self
-
-	def list(self, key):
-		if (self.__isObjNeed()):
-                        self.items.append(Obj())
-		if (1 == self.__isNextNeed()):
-                        self.items.append(Next())
-		self.items.append(List(key))
+		self.items.append(KeyValue(key, value))			
 		return self
 
 	def close(self):
 		if (self.__isObjNeed()):
-                        self.items.append(Obj())
+			level = self.__findAndRaiseCurrentLevel()
+                        self.items.append(Obj(level))
 		val = ""
 		if (isinstance(self.items[-1], List)):
 			val = "]"
@@ -44,7 +55,8 @@ class JsonBuilder(object):
 
 	def closeAll(self):
 		if (self.__isObjNeed()):
-                       self.items.append(Obj())
+			level = self.__findAndRaiseCurrentLevel()
+                       	self.items.append(Obj(level))
 		self.items.append(CloseAll())
 		return self
 	
@@ -52,63 +64,104 @@ class JsonBuilder(object):
 		x = ""
 		for item in self.items:
 			if (item is not None):
-				print item
 				if (isinstance(item, Obj)):
+					if (self.pretty):
+						if (0==len(x)):
+	                                                self.level = item.level
+						x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
 					x = x + "{"
-					if (self.pretty == 1):
-                                	        x= x + "\n"
+					if(self.pretty):
+                                		self.level = self.level + 1
+			                        x = x + "\n"
+
 
 				if isinstance(item, List):
-					if (self.pretty ==1):
-						x = x + "\n"
+					if (self.pretty):
+						x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
 					x = x + "\"" +  item.key + "\": ["
-					if (self.pretty == 1):
-						x= x + "\n"
+					if (self.pretty):
+						self.level = self.level + 1
+                                                x = x + "\n"
 
 				if(isinstance(item, KeyValue)):	
-					x = x + self.__writekv(item.key, item.value)
+					if (self.pretty):
+                                                x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
+					x = x + "\"" + item.key + "\": \"" + str(item.value) + "\""
 
 				if(isinstance(item, Value)):
-                                        x = x + "\"" +  item.value + "\""
+                                        if (self.pretty):
+                                                x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
+                                        x = x + "\"" +  str(item.value) + "\""
 
 				if (isinstance(item, Close)):
+                                        if (self.pretty):
+						self.level = self.level - 1
+					        x = x + "\n"
+						x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
                                 	x = x + item.value
+					if (self.pretty):
+                                                x = x + "\n"
+
 				
 				if (isinstance(item, Next)):
                                         x = x + item.value
+					if (self.pretty):
+                                                x = x + "\n"
 
 				if (isinstance(item, CloseAll)):
-					for item in reversed(self.items):
-						if (isinstance(item, Obj) or isinstance(item, List)):
-							if (self.pretty ==1):
-                                                                x = x + "\n"
-
-							if (isinstance(item, Obj)):
-								x = x + "}"
+					ignorenext = 0
+					once = 0
+					for it in reversed(self.items):
+						if (isinstance(it, Close)):
+							ignorenext = ignorenext + 1
+						if (isinstance(it, Obj) or isinstance(it, List)):
+							if (0 < ignorenext):
+								ignorenext = ignorenext - 1
 							else:
-								x = x + "]"
-							
+								if (self.pretty):
+									self.level = self.level - 1
+									# Buggy.... noch fixen
+									if(0 == once and (isinstance(self.items[-2], Value) or isinstance(self.items[-2], KeyValue))):
+										x = x + "\n"
+										once = 1
+									x = x + "\t".expandtabs(self.level*JsonBuilder.PRETTY_DIST)
+	
+								if (isinstance(it, Obj)):
+									x = x + "}"
+								else:
+									x = x + "]"
+								if (self.pretty):
+                                                                        x = x + "\n"
 
+				print(item, self.level)
+		self.level = 0
 
 		return x
-
-	def __writekv(self, key, value):
-		x = ""
-		if (self.pretty == 1):
-                       	x = x + "\n"
-                x = x + "\"" + key + "\": \"" + value + "\""
-		return x
-
+	
 	def __isObjNeed(self):
-		if (0 == len(self.items) or isinstance(self.items[-1], List) or isinstance(self.items[-1], Close)):
+		if (0 == len(self.items) or isinstance(self.items[-1], List) or isinstance(self.items[-1], Next)):
 			return 1
-		return 0
-		
+		return 0		
 	
 	def __isNextNeed(self):
-		if (isinstance(self.items[-1], Value) or isinstance(self.items[-1], KeyValue)):
+		if (isinstance(self.items[-1], Value) or isinstance(self.items[-1], KeyValue) or isinstance(self.items[-1], Close)):
 			return 1
 		return 0
+
+	def __findAndRaiseCurrentLevel(self):
+		return self.__findCurrentLevel() + 1
+
+	def __findCurrentLevel(self):
+		if (0 == len(self.items)):
+			return -1
+		else:
+			level = 0
+			for item in self.items:
+				if (isinstance(item, List) or isinstance(item, Obj)):
+					level = level + 1
+				if (isinstance(item, Close)):
+					level = level - 1
+			return level
 
 	def _classtest(self, cls):
 		if(isinstance(cls, KeyValue)):
@@ -117,8 +170,16 @@ class JsonBuilder(object):
 			
 class Obj(object):
 
-	def __init__(self):
+	def __init__(self, level):
+		self.level = level
 		self.value = "{"
+
+class List(object):
+
+        def __init__(self, key, level):
+		self.level = level
+                self.key = key
+                self.value = "["
 
 class Value(object):
 	
@@ -130,12 +191,6 @@ class KeyValue(object):
 	def __init__(self, key, value):
 		self.key = key
 		self.value = value
-
-class List(object):
-	
-	def __init__(self, key):
-		self.key = key
-		self.value = "["
 
 class Next(object):
 
--- test_JsonBuilder.py
+++ test_JsonBuilder.py
@@ -6,37 +6,54 @@ from JsonBuilder import List
 
 class Test_test_JsonBuilder(unittest.TestCase):
 
-        def test_builder_create(self):
+	def test_builder_create(self):
                 a_builder = JsonBuilder(1)
                 self.assertEquals(a_builder.pretty, 1)
 
-	def test_builder_classtest(self):
-		a_builder = JsonBuilder(0)
-		a_keyvalue = KeyValue("a","b")
-		a_list = List("a")
+        def test_builder_classtest(self):
+		print "A simple Classmatching Test"
+                a_builder = JsonBuilder(0)
+                a_keyvalue = KeyValue("a","b")
+                a_list = List("a", 0)
                 print a_builder._classtest(a_keyvalue)
-		print a_builder._classtest(a_list)
+                print a_builder._classtest(a_list)
 
-	def test_builder_simpel(self):
+        def test_builder_simple(self):
+		print "A simple Builder Test with KeyValues"
                 a_builder = JsonBuilder(0)
                 a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
                 print a_builder.build()
-	
-	def test_builder_pretty_simple(self):
-		a_builder = JsonBuilder(1)
-		a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
-		print a_builder.build()
+
+        def test_builder_pretty_simple(self):
+		print "A simple Builder Test with KeyValues in prittyprint"
+                a_builder = JsonBuilder(1)
+                a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
+                print a_builder.build()
 
         def test_builder_advanced(self):
+		print "An advanced Builder Test with KeyValues and Lists"
                 a_builder = JsonBuilder(0)
                 a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").list("a_list").keyvalue("a_lkey", "alvalue").closeAll()
                 print a_builder.build()
 
-
         def test_builder_pretty_advanced(self):
-                a_builder = JsonBuilder(1)
-                a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").close()
+		print "An advanced Builder Test with KeyValues and Lists in prittyprint"
+		a_builder = JsonBuilder(1)
+                a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").list("a_list").keyvalue("a_lkey", "alvalue").closeAll()
                 print a_builder.build()
+	
+	def test_builder_pretty_advanced_close(self):
+                print "An advanced Builder Test with KeyValues a List and a Close  in prittyprint"
+                a_builder = JsonBuilder(1)
+                a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").list("a_list").keyvalue("a_lkey", "alvalue").close().closeAll()
+                print a_builder.build()
+
+	def test_builder_pretty_advanced_multielist_close(self):
+                print "An advanced Builder Test with KeyValues and Lists and a Close  in prittyprint"
+                a_builder = JsonBuilder(1)
+                a_builder.keyvalue("a_key","a_value").keyvalue("b_key","b_value").list("a_list").keyvalue("a_lkey", "alvalue").close().list("b_list").keyvalue("b_lkey","b_lvalue").closeAll()
+                print a_builder.build()
+
 
 if __name__ == '__main__':
     unittest.main()
